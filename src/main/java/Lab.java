e13fe83149c5ea4f43c9fa4d1b998e75e5635517
Brian Yang <yangbri1@msu.edu>
2024-12-04 01:11:51 +0000

    Fulfilled MVP

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -24,7 +24,7 @@ public class Lab {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        return (a == b);
     }
 
     /**
@@ -33,7 +33,7 @@ public class Lab {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        return (a > b);
     }
     /**
      * @param a the first number to be compared.
@@ -41,7 +41,7 @@ public class Lab {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        return (a < b);
     }
     /**
      * @param a the first number to be compared.
@@ -49,7 +49,7 @@ public class Lab {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        return (a >= b);
     }
     /**
      * @param a the first number to be compared.
@@ -57,7 +57,7 @@ public class Lab {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        return (a <= b);
     }
 
     /**
@@ -66,6 +66,6 @@ public class Lab {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        return (a != b);
     }
 }
